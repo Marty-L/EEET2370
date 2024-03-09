35c3c6e0634cfb11357beda8c3eb5df7be22f065
Marty Lawrence <s3788137@student.rmit.edu.au>
2024-03-09 20:44:21 +1100

    Fixing master/main ref naming mixup...thanks ChatGPT!

---
+++ Lab1Code/blinkWithDelay/blinkWithDelay.ino
@@ -0,0 +1,17 @@
+//GLOBAL VARIABLES
+unsigned long freq = 10; //Desired LED blink frequency (Hz)
+unsigned long HALF_PERIOD_MILLIS = long(abs((0.5)*(1.0/freq))); //Half the period (ms) - cast for safety when using delay()
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);       //Set built-in LED port as output.
+  
+  Serial.begin(9600);                 //Set up serial port for logging output.
+  Serial.println(HALF_PERIOD_MILLIS); //Echo to serial log to check value.
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);    //LED ON
+  delay(HALF_PERIOD_MILLIS);
+  digitalWrite(LED_BUILTIN, LOW);     //LED OFF
+  delay(HALF_PERIOD_MILLIS);
+}
