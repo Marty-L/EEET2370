35c687dde6f05a4cb4cfbf92af3687d1759248d0
Marty Lawrence <s3788137@student.rmit.edu.au>
2024-03-09 21:04:10 +1100

    Squashed bug in delay calc - need to x 1000 for ms

--- Lab1Code/blinkWithDelay/blinkWithDelay.ino
+++ Lab1Code/blinkWithDelay/blinkWithDelay.ino
@@ -1,6 +1,6 @@
 //GLOBAL VARIABLES
 unsigned long freq = 10; //Desired LED blink frequency (Hz)
-unsigned long HALF_PERIOD_MILLIS = long(abs((0.5)*(1.0/freq))); //Half the period (ms) - cast for safety when using delay()
+unsigned long HALF_PERIOD_MILLIS = long(abs((0.5)*(1.0/freq))*1000); //Half the period (ms) - cast for safety when using delay()
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);       //Set built-in LED port as output.
