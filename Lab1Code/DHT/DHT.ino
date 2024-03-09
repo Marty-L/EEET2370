b396932001010d1106a1c1ddb060c4a98959d0aa
Marty Lawrence <s3788137@student.rmit.edu.au>
2024-03-09 21:20:39 +1100

    Add DHT skeleton code for Lab 1

---
+++ Lab1Code/DHT/DHT.ino
@@ -0,0 +1,19 @@
+#include <dht.h>
+
+dht DHT;
+
+//GLOBAL VARIABLES
+#define DHT11_PIN 5
+
+void setup() {
+  Serial.begin(9600);                 //Set up serial port for logging output.
+}
+
+void loop() {
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(2000);
+}
